ead0d8c13ffc019de32402f06bce04b991080b1a
Joanna Hauza <joannahauza@gmail.com>
2019-08-10 18:37:36 +0200

    initial commit

--- src/main/java/pl/coderslab/model/DbUtil.java
+++ src/main/java/pl/coderslab/model/DbUtil.java
@@ -1,3 +1,5 @@
+package pl.coderslab.model;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
---
+++ src/main/java/pl/coderslab/model/UserDAO.java
@@ -0,0 +1,146 @@
+package pl.coderslab.model;
+
+import pl.joanna026.progschool.model.User;
+import pl.joanna026.progschool.util.DBUtil;
+
+import java.sql.*;
+import java.util.Arrays;
+
+public class UserDAO {
+
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users (email, username, password, user_group_id) VALUES (?, ?, ?, ?)";
+    private static final String READ_USER_QUERY = "SELECT * FROM users WHERE id = ?";
+    private static final String UPDATE_USER_QUERY =
+            "UPDATE users SET email=?, username=?, password=?, user_group_id=? WHERE id=?";
+    private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id=?";
+    private static final String FIND_ALL_USERS_QUERY = "SELECT * FROM users";
+    private static final String FIND_ALL_BY_GROUP_QUERY= "SELECT * FROM users WHERE group_id=?";
+
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+
+            //Ustawienie parametrów zapytania (wartości do wstawienia)
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getUsername());
+            statement.setString(3, user.getPassword());
+            statement.setInt(4, user.getUser_group_id());
+
+            //Wykonanie zapytania
+            statement.executeUpdate();
+
+            // Pobranie zestawu wygenerowanych kluczy
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.first()) {
+                int generatedKey = generatedKeys.getInt(1);
+                user.setId(generatedKey);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    public User read(int id) {
+
+        try (Connection conn = DbUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(READ_USER_QUERY);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                user.setUser_group_id(resultSet.getInt("user_group_id"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void update(User user) {
+        try (Connection conn = DbUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(UPDATE_USER_QUERY);
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getUsername());
+            statement.setString(3, user.getPassword());
+            statement.setInt(4, user.getUser_group_id());
+            statement.setInt(5, user.getId());
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void delete(int id) {
+        try (Connection conn = DbUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(DELETE_USER_QUERY);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User[] findAll() {
+        try (Connection conn = DbUtil.connect()) {
+            User[] users = new User[0];
+            PreparedStatement statement = conn.prepareStatement(FIND_ALL_USERS_QUERY);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                user.setUser_group_id(resultSet.getInt("user_group_id"));
+                users = addToArray(user, users);
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1);
+        tmpUsers[users.length] = u;
+        return tmpUsers;
+    }
+
+    public User[] findAllByGroup(int group_id){
+        try (Connection conn = DbUtil.connect()) {
+            User[] users = new User[0];
+            PreparedStatement statement = conn.prepareStatement(FIND_ALL_BY_GROUP_QUERY);
+            statement.setInt(1,group_id);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                user.setUser_group_id(resultSet.getInt("user_group_id"));
+                users = addToArray(user, users);
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
